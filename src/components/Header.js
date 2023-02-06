9df6e2eddc15e916ef39128c642120cc365327cf
Aubriot Marc <marc.aubriot@outlook.fr>
2023-02-06 09:34:24 +0100

    data structure, components, ressources

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Logo from './Logo.js';
+import Nav from './Nav.js';
+
+const Header = () => {
+
+    return (
+        <header>
+            <Logo />
+            <Nav />
+        </header>
+    );
+}
+
+export default Header;
