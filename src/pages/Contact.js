1e555f24e560bbcd6e97d390f84fc547e7569f83
Aubriot Marc <marc.aubriot@outlook.fr>
2023-02-07 15:35:31 +0100

    page contact layout

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,34 @@
 import React, { useState } from "react";
+import './styles/Contact.css';
+import Footer from "../components/Footer";
 
 const Contact = () => {
     return (
-        <>
-            <h1>Contact TEST</h1>
-        </>
+        <main className="mainStyles">
+            <h1 className="sectionTitle">Contact</h1>
+
+            <section className="contact">
+
+                <div className="formWrapper">
+                        
+                    <form className="formArea">
+
+                        <label className="emailArea">
+                            <input type="email" name="email" className="emailInputArea" required/>
+                        </label>
+                        
+                        <textarea value="Message" className="formTextArea" required/>
+
+                        <button type="submit" className="submitBtn">Envoyer </button>
+
+                    </form>
+
+                </div>
+
+            </section>
+
+            <Footer />
+        </main>
     );
 }
 
