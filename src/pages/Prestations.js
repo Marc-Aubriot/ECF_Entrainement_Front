d3e257b5a30ceb10056981c80072deb51ad058c8
Aubriot Marc <marc.aubriot@outlook.fr>
2023-02-09 18:47:44 +0100

    bugfix

--- src/pages/Prestations.js
+++ src/pages/Prestations.js
@@ -22,7 +22,7 @@ const Prestations = () => {
     const cards = [
         ["Juste moi", pic_justemoi, "130€", "Séance pour une personne en extérieur ou en studio.","#9CA4A4"],
         ["Pour deux", pic_pourdeux, "195€", "Pour deux personnes, en extérieur ou en studio.","#44645C"],
-        ["Famille", pic_famille, "220€", "Pour la famille ou les amis jusqu'à 4 personnes, en extérieur ou en studio. 30 euros en supllément par personne au-delà de 4 (hormis enfant jusqu'à 2ans).","#6C60A8"],
+        ["Famille", pic_famille, "220€", "Pour la famille ou les amis jusqu'à 4 personnes, en extérieur ou en studio. 30 euros en supllément par personne au-delà de 4 (hormis enfant jusqu'à 2ans).","#9CA4A4"],
         ["Il était une fois", pic_iletaitunefois, "160€", "Photo de grossesse (À votre domicile, en extérieur ou en studio).","#70605C"],
         ["Mon bébé", pic_monbebe, "100€", "Photo d'enfant jusqu'à 3 ans (photo à domicile).","#9CA4A4"],
         ["J'immortalise l'évènement", pic_jimmortalise, "Sur mesure", "Prestation de mariage ou baptême sur devis.","#6C60A8"],
