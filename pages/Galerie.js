024021d06410ab299c837e71c58a0a2043e13cc7
Aubriot Marc <marc.aubriot@outlook.fr>
2023-02-14 09:51:00 +0100

    branch fix, import fix, added json

--- pages/Galerie.js
+++ pages/Galerie.js
@@ -6,13 +6,12 @@ import Footer from './components/Footer.js';
 import Header from "./components/Header.js";
 import Meta from "./components/Meta.js";
 //import filesToLoad from "../public/ressources/filesToLoad.json";
-//import file1 from "../public/ressources/piclist.json";
+import file1 from "../public/ressources/piclist.json";
 
 /* Page Galerie */
 const Galerie = () => { 
 
     const picsList = file1.picList;
-    const json = async () => { return import('../public/ressources/piclist.json'); }
 
     /* dropdown hooks */
     const [dropdownOpen, setDropdownOpen] = useState(false);
